ab6559e477204c61901f7dc1d6699e627a0cecbe
min <mini6129@nate.com>
2013-03-17 23:39:35 +0900

    abc commit

--- abc/src/test.java
+++ abc/src/test.java
@@ -4,7 +4,7 @@ public class test {
         System.out.println("매개변수의 개수 : " + args.length);
         for(int i=0; i < args.length; i++){
             System.out.println("args[" + i + "] = \""+ args[i] + "\"");
-            
+            System.out.println("args[" + i + "] = \""+ args[i] + "\"");
         }
     }
 }
